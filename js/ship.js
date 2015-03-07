d28e5660535b92d28dbc1fd1466bb4e953cc3893
juan mamani <jjmd.dj@gmail.com>
2015-03-07 03:08:59 +0100

    was added some comments in some methods

--- js/ship.js
+++ js/ship.js
@@ -1,5 +1,9 @@
-// Ship class is to model the ship
-
+/**
+ * Ship class is to model the ship
+ * @param {number} size        The size ship
+ * @param {number} orientation The position vertical or horizontal
+ * @param {number} shipPos     The position for the ship
+ */
 var Ship = function(size, orientation, shipPos){ // sipPost = {r:1, c:2}
 	this.size = size;
 	this.status = 'ALIVE';
@@ -7,12 +11,18 @@ var Ship = function(size, orientation, shipPos){ // sipPost = {r:1, c:2}
 	this.position = shipPos;
 	this.hits = 0;
 
-	//verify if the ship is alive
+	/**
+	 * Verify if the ship is alive
+	 * @return {Boolean} return wether the ship is alive
+	 */
 	this.isAlive = function() {
 		return this.hits < this.size;
 	};
 
-	//verify hit status
+	/**
+	 * Verify hit status
+	 * @return {windows} display an alert messaje when the ship was destroyed.
+	 */
 	this.hit = function() {
 		this.hits++;
 		if (this.isAlive()) {
