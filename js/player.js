4b0b6dae387bf60047a4589cf57eb7558588253c
juan mamani <jjmd.dj@gmail.com>
2015-03-07 02:54:21 +0100

    was added comments in the same methods

--- js/player.js
+++ js/player.js
@@ -1,20 +1,33 @@
-//player class is to model to players
-
+/**
+ * Player class is to model to players
+ * @param {object} battleField the object battle field
+ * @param {string} name the name the player
+ */
 var Player = function(battleField, name){
 	this.battleField = battleField;
 	// this.name = window.prompt('Your name?');
 	this.name = name;
 	
-	//model the shot that will be performed since player
+	/**
+	 * Model the shot that will be performed since player
+	 * @param  {[type]} row row This is a position for row
+	 * @param  {[type]} col col This is a position for rcolumn
+	 */
 	this.shot = function (row, col) {
 		this.battleField.performShot(row, col);
 	};
 
-	//Verify if the field has ship alive
+	/**
+	 * Verify if the field has ship alive
+	 * @return {Boolean} return true wether the ship alive
+	 */
 	this.hasShipsAlive = function() {
 		return this.battleField.hasShipsAlive();
 	}
 
+	/**
+	 * Print the Battle field
+	 */
 	this.printBattleField = function() {
 		console.log(this.name + '\'s battlefield:');
 		this.battleField.print();
